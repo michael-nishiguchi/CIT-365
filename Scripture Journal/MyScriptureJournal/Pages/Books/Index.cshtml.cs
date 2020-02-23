00e6f41415b837d0edabdd0d998efe59d135d4ef
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-02-22 19:42:34 -0700

    Scripture journal

---
+++ Scripture Journal/MyScriptureJournal/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyScriptureJournal.Models;
+
+namespace MyScriptureJournal.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyScriptureJournal.Models.MyScriptureJournalContext _context;
+
+        public IndexModel(MyScriptureJournal.Models.MyScriptureJournalContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string NoteString { get; set; }
+
+
+        public async Task OnGetAsync()
+        {
+            var books = from b in _context.Book
+                         select b;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                books = books.Where(s => s.BookName.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(NoteString))
+            {
+                books = books.Where(s => s.Note.Contains(NoteString));
+            }
+
+            Book = await books.ToListAsync();
+        }
+    }
+}
